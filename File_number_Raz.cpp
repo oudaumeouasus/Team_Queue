5805d3e7d8610396e6bdee227813347ecdbd9385
Grichin <80479142+Grichin@users.noreply.github.com>
2024-04-27 20:40:30 +0300

    Update File_number_Raz.cpp

--- File_number_Raz.cpp
+++ File_number_Raz.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-hhhhhhhjjjjj
+#include "Queue.h"
+#include "Man.h"
+
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "ru");
+	Queue A;
+	int n;
+	cout << "Количество: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		A.add_con(i+1);
+	}
+
+	A.print();
+
+	return 0;
 }
 
