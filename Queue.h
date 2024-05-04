71a278ccdf7dac6939780085a53118e21f334c60
Vo0DoChild <167576191+Vo0DoChild@users.noreply.github.com>
2024-05-04 22:43:11 +0300

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -3,38 +3,38 @@
 using namespace std;
 #pragma once
 class Queue {
-	friend std::ostream& operator<<(std::ostream& output, Queue& q);
-	
+  friend std::ostream& operator<<(std::ostream& output, Queue& q);
+
 private:
-	Man* head;
-	Man* tail;
-	int count;
+  Man* head;
+  Man* tail;
+  int count;
 public:
-	void set_count(int new_count);
-	void set_head(Man *new_head);
-	void set_tail(Man *new_tail);
+  void set_count(int new_count);
+  void set_head(Man *new_head);
+  void set_tail(Man *new_tail);
 
-	int get_count() const;
-	Man* get_head() const;
-	Man* get_tail() const;
+  int get_count() const;
+  Man* get_head() const;
+  Man* get_tail() const;
 
-	void add_con(float value);//добавление в конец
-	void add_nach(float value);//добавление в начало
-	void del_head();//удаление первого элемента(головы)
-	void del_all();//удаление всей очереди
-	void del_index(int index);//удаление элемета по индексу
-	void del_znach(float znach);//удаление значения по значению
-	float del_info(float znach);//удаление с выводом значения
-	void change(float old, float new_value);//замена элемента
-	void tek_znach();//узнать текущее значение 
-	void inversion();//инвертирование
-	
+  void add_con(float value);//добавление в конец
+  void add_nach(float value);//добавление в начало
+  void del_head();//удаление первого элемента(головы)
+  void del_all();//удаление всей очереди
+  void del_index(int index);//удаление элемета по индексу
+  void del_znach(float znach);//удаление значения по значению
+  float del_info(float znach);//удаление с выводом значения
+  void change(float old, float new_value);//замена элемента
+  void tek_znach();//узнать текущее значение 
+  void inversion();//инвертирование
+  void del_copy();//удаление дубликатов
 
-	bool isEmpty() const;
-	Queue();
-	Queue(int count);
-	~Queue();
-	void print();
+  bool isEmpty() const;
+  Queue();
+  Queue(int count);
+  ~Queue();
+  void print();
 
-	float& operator[](const int index);
+  float& operator[](const int index);
 };
