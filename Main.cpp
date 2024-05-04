1f3bbe15fe8dcede80392ce7a54132506660aa73
Vo0DoChild <167576191+Vo0DoChild@users.noreply.github.com>
2024-05-04 22:42:09 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,32 +2,38 @@
 #include "Queue.h"
 #include "Man.h"
 
+
 using namespace std;
 int main() {
-	setlocale(LC_ALL, "ru");
-	Queue A;
-	int n;
-	cout << "Количество: ";
-	cin >> n;
+  setlocale(LC_ALL, "ru");
+  Queue A;
+  int n;
+  cout << "Количество: ";
+  cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		A.add_con(i + 1);
-	}
-	for (int i = 0; i < n; i++)
-	{
-		A.add_con(i + 1);
-	}
-	cout << A;
-	cout << endl;
-	A.del_index(2);
-	A.add_con(8);
-	cout << A;
-	A.tek_znach();
-	cout<< A.del_info(4)<<endl;
-	cout << A;
-	A.tek_znach();
-	A.inversion();
-	cout << A;
-	return 0;
+  for (int i = 0; i < n; i++)
+  {
+    A.add_con(i + 1);
+  }
+  for (int i = 0; i < n; i++)
+  {
+    A.add_con(i + 1);
+  }
+  cout << A;
+  cout << endl;
+  A.del_index(2);
+  A.add_con(8);
+  cout << A;
+  A.tek_znach();
+  if(A.del_info(4))
+    {}
+  else
+    cout << "Такого элемента нет"<<endl;
+  cout << A;
+  A.tek_znach();
+  A.inversion();
+  cout << A;
+  A.del_copy();
+  cout << A;
+  return 0;
 }
